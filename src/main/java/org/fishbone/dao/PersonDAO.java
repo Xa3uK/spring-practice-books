e0abb636edcb846e856d747dd4ddde17c78583a9
Koval <hazardsales@gmail.com>
2022-05-17 17:01:20 +0300

    Batch update added

--- src/main/java/org/fishbone/dao/PersonDAO.java
+++ src/main/java/org/fishbone/dao/PersonDAO.java
@@ -1,8 +1,12 @@
 package org.fishbone.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import org.fishbone.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -35,11 +39,56 @@ public class PersonDAO {
     }
 
     public void update(int id, Person personUpdated) {
-      jdbcTemplate.update("update person set name=?, age=?, email=? where id=?", personUpdated.getName(),
-          personUpdated.getAge(), personUpdated.getEmail(), id);
+        jdbcTemplate.update("update person set name=?, age=?, email=? where id=?", personUpdated.getName(),
+            personUpdated.getAge(), personUpdated.getEmail(), id);
     }
 
     public void delete(int id) {
         jdbcTemplate.update("delete from person where id=?", id);
+    }
+
+    public void testMultipleUpdate() {
+        List<Person> people = create1000People();
+        long before = System.currentTimeMillis();
+
+        for (Person person : people) {
+            jdbcTemplate.update("insert into person values(?,?,?,?)", person.getId(), person.getName(),
+                person.getAge(),
+                person.getEmail());
+        }
+
+        long after = System.currentTimeMillis();
+
+        System.out.println(after - before);
+    }
+
+    private List<Person> create1000People() {
+        List<Person> people = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            people.add(new Person(i, 30, "Name" + i, "test" + i + "@mail.ua"));
+        }
+        return people;
+    }
+
+    public void testBatchUpdate() {
+        List<Person> people = create1000People();
+
+        long before = System.currentTimeMillis();
+        jdbcTemplate.batchUpdate("insert into person values(?,?,?,?)", new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setInt(1, people.get(i).getId());
+                ps.setString(2, people.get(i).getName());
+                ps.setInt(3, people.get(i).getAge());
+                ps.setString(4, people.get(i).getEmail());
+            }
+
+            @Override
+            public int getBatchSize() {
+                return people.size();
+            }
+        });
+        long after = System.currentTimeMillis();
+        System.out.println(after - before);
     }
 }
